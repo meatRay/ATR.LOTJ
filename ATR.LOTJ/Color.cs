e5c58b46fed0945dc8f68466704ee5a1f45c1e26
Shannon Jon Francis Cavanagh <Stroggs@live.com>
2023-10-19 16:17:23 -0500

    orientations. at thing point... just need a b

--- ATR.LOTJ/Color.cs
+++ ATR.LOTJ/Color.cs
@@ -10,6 +10,8 @@ namespace ATR.LOTJ;
 
 readonly record struct ColorSpace(int Red, int Green, int Blue)
 {
+    public static ColorSpace Sign(in ColorSpace space)
+        => new(int.Sign(space.Red), int.Sign(space.Green), int.Sign(space.Blue));
     public static int Magnitude(in ColorSpace space)
         => int.Abs(space.Red) + int.Abs(space.Green) + int.Abs(space.Blue);
     public static ColorSpace Abs(in ColorSpace space)
@@ -30,11 +32,16 @@ readonly record struct ColorSpace(int Red, int Green, int Blue)
         => new(left.Red - right.Red, left.Green - right.Green, left.Blue - right.Blue);
     public static ColorSpace operator +(in ColorSpace left, in ColorSpace right)
         => new(left.Red + right.Red, left.Green + right.Green, left.Blue + right.Blue);
+    public static ColorSpace operator *(in ColorSpace left, int scalar)
+        => new(left.Red * scalar, left.Green * scalar, left.Blue * scalar);
 }
 
-enum ColorDitherStyle { RedGreenBlue, BiggerSmaller, Even }
+//enum ColorDitherStyle { RedGreenBlue, BiggerSmaller, Even }
+enum ColorOrient { RedGreen, RedBlue, GreenBlue, GreenRed, BlueRed, BlueGreen }
 // eh way more maintainable to mutate some state
-record struct ColorDither(ColorSpace Left, ColorSpace Right, int Length)
+record struct ColorDither(in ColorSpace Left, in ColorSpace Right,
+    int Length,
+    ColorOrient Orient = ColorOrient.RedGreen)
 {
     public readonly ColorSpace Range => Right - Left;
     public int At { get; private set; } = 0;
@@ -60,6 +67,22 @@ record struct ColorDither(ColorSpace Left, ColorSpace Right, int Length)
         return Left + new ColorSpace(rspan, gspan, bspan);
     }
 
+    readonly ColorSpace ColorOne => Orient switch {
+        ColorOrient.RedGreen or ColorOrient.RedBlue => new(Range.Red, 0, 0),
+        ColorOrient.GreenBlue or ColorOrient.GreenRed => new(0, Range.Green, 0),
+        _ => new(0, 0, Range.Blue),
+    };
+    readonly ColorSpace ColorTwo => Orient switch {
+        ColorOrient.GreenRed or ColorOrient.BlueRed => new(Range.Red, 0, 0),
+        ColorOrient.RedGreen or ColorOrient.BlueGreen => new(0, Range.Green, 0),
+        _ => new(0, 0, Range.Blue),
+    };
+    readonly ColorSpace ColorThree => Orient switch {
+        ColorOrient.GreenBlue or ColorOrient.BlueGreen => new(Range.Red, 0, 0),
+        ColorOrient.RedBlue or ColorOrient.BlueRed => new(0, Range.Green, 0),
+        _ => new(0, 0, Range.Blue),
+    };
+
     public ColorSpace SmallandMove()
     {
         // a param for the ORDER of the colors ( 
@@ -72,15 +95,41 @@ record struct ColorDither(ColorSpace Left, ColorSpace Right, int Length)
         if (At + 1 == Length)
             return Right;
         var dif = Range;
-        var difr = int.Abs(dif.Green);
-        var difg = int.Abs(dif.Red) + difr;
-        var difb = int.Abs(dif.Blue) + difg + difr;
-        if (At <= difr)
-            return Left + new ColorSpace(0, int.Sign(Range.Green) * (At++ - 0), 0);
-        else if (At <= difg)
-            return Left + new ColorSpace(int.Sign(Range.Red) * (At++ - difr), Range.Green, 0);
+        var size = ColorSpace.Abs(Range);
+        (int split1, int split2) = Orient switch {
+            ColorOrient.RedGreen => (size.Red, size.Green),
+            ColorOrient.RedBlue => (size.Red, size.Blue),
+            ColorOrient.GreenBlue => (size.Green, size.Blue),
+            ColorOrient.GreenRed => (size.Green, size.Red),
+            ColorOrient.BlueGreen => (size.Blue, size.Green),
+            _ => (size.Blue, size.Red),
+        };
+
+        int at1 = At - (0),
+            at2 = At - (split1),
+            at3 = At - (split1 + split2);
+
+        ColorSpace done = default;
+        int useat;
+        if (at1 <= split1)
+        {
+            done += ColorSpace.Sign(ColorOne) * at1;
+            useat = at1;
+        }
+        else if (at2 <= split2)
+        {
+            done += ColorSpace.Sign(ColorTwo) * at2;
+            done += ColorOne;
+            useat = at2;
+        }
         else
-            return Left + new ColorSpace(Range.Red, Range.Green, int.Sign(Range.Blue) * (At++ - difg));
+        {
+            done += ColorSpace.Sign(ColorThree) * at3;
+            done += ColorOne + ColorTwo;
+            useat = at3;
+        }
+        ++At;
+        return Left + done;
         /*var difr = int.Abs(dif.Green);
         var difg = int.Abs(dif.Blue) + difr;
         var difb = int.Abs(dif.Red) + difg + difr;
